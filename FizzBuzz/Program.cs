73ab645c919d49f2fa514174b9cbeced1f507921
Daniel Bevan <20234823+Dbevan770@users.noreply.github.com>
2024-01-16 16:27:49 +0100

    My Solution to FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿// Normal version
+Console.WriteLine("FizzBuzz -- Normal Version");
+for (int i = 1; i <= 100; ++i)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
+// Ternary version
+Console.WriteLine("FizzBuzz -- Ternary Version");
+for (int t = 1; t <= 100; ++t) Console.WriteLine(t % 5 == 0 && t % 3 == 0 ? "FizzBuzz" : t % 5 == 0 ? "Buzz" : t % 3 == 0 ? "Fizz" : t.ToString());
